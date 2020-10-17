1046f240a768a96ed2717ce070669fcc84114549
Eloy Mazza <eloymazza.ra@gmail.com>
2020-10-17 00:43:15 -0300

    agregue comments form

--- tp3Components/form/main.js
+++ tp3Components/form/main.js
@@ -1,5 +1,9 @@
+const commentsForm = document.querySelector('.comments-form')
 const batiScores = document.querySelectorAll('.bati-score')
 const sendButton = document.querySelector('#send-button')
+const batmanLogocontainer = document.querySelector('.batman-logo-container')
+const commentSentContainer =  document.querySelector('.comment-sent-container')
+const sendAgainButton =  document.querySelector('#send-again-button')
 
 const vote = (level) => {
     for (let i = 0; i < batiScores.length; i++) {
@@ -17,8 +21,37 @@ batiScores.forEach( (batiScore, i) => {
     batiScore.addEventListener('click', () => vote(i) )
 })
 
-const sendMessage = () => {
-    sendButton.classList.add("send-message")
+
+const sendComment = () => {
+    sendButton.classList.add("hide-send-button")
+    setTimeout(showCommentsLogo, 2000)
 }
 
-sendButton.addEventListener("click", sendMessage)
+const showCommentsLogo = () => {
+    batmanLogocontainer.style.opacity = 1
+    setTimeout(closeCommentsForm, 2000)
+}
+
+const closeCommentsForm = () => {
+    commentsForm.classList.add('close-comments-form')
+    batmanLogocontainer.classList.add('move-up-logo')
+    setTimeout(() => {
+        commentSentContainer.style.zIndex = 5
+    }, 2000)
+}
+
+const sendCommentAgain = () => {
+    commentSentContainer.style.zIndex = -1
+    commentsForm.classList.remove('close-comments-form')
+    batmanLogocontainer.classList.remove('move-up-logo')
+    setTimeout(() => {
+        batmanLogocontainer.style.opacity = 0
+        setTimeout(() => {
+            sendButton.classList.remove("hide-send-button")
+        }, 2000)
+    }, 2000)
+}
+
+
+sendButton.addEventListener("click", sendComment)
+sendAgainButton.addEventListener('click', sendCommentAgain)
