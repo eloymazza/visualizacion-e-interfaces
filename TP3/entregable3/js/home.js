ac95426b9ee670febd9de4ff75edcdddd2be6f11
Eloy Mazza <eloymazza.ra@gmail.com>
2020-10-18 19:41:33 -0300

    arreglados issues visualeS

--- TP3/entregable3/js/home.js
+++ TP3/entregable3/js/home.js
@@ -4,6 +4,7 @@ const loadHomeJS = () => {
     const paralaxContainer = document.querySelector(".paralax-container") 
     const countdownContainer = document.querySelector(".countdown-container") 
     const castGridContainer = document.querySelector(".cast-grid-container") 
+    const sliderContainer = document.querySelector(".slider-container") 
 
     document.addEventListener("wheel", (e) => {
         if(e.deltaY > 0) {
@@ -19,7 +20,10 @@ const loadHomeJS = () => {
         if(scrollLevel > 0 && scrollLevel < 11) assignParalaxTransition()
         else if (scrollLevel >= 11 && scrollLevel < 20) assignCoundownEntrance() 
         else if (scrollLevel >= 20 && scrollLevel < 23) assignCoundownExit()
-        else if(scrollLevel > 28 && scrollLevel < 32) assignGridTransition() 
+        else if(scrollLevel > 24 && scrollLevel < 32) assignGridEntrance() 
+        else if(scrollLevel >= 33 && scrollLevel < 42) assignGridExit() 
+        else if(scrollLevel >= 42) assignSliderEntrance() 
+
     }
 
     const assignParalaxTransition = () => [
@@ -34,10 +38,19 @@ const loadHomeJS = () => {
         countdownContainer.classList.remove('fade-in')
     }
 
-    const assignGridTransition = () => {
+    const assignGridEntrance = () => {
         castGridContainer.classList.add('slide-in')
     }
 
+    const assignGridExit = () => {
+        castGridContainer.classList.remove('slide-in')
+    }
+
+    const assignSliderEntrance = () => {
+        sliderContainer.classList.add('appear-in-center')
+    }
+
+    
     // Countdown
     
     const countdown = document.querySelector(".countdown")
@@ -78,32 +91,33 @@ const loadHomeJS = () => {
         setCountdown()
     }, 1000);
 
+
+
+    /* Slider */ 
+
+    const leftButton = document.querySelector('#left-button')
+    const rightButton = document.querySelector('#right-button')
+    const images = document.querySelectorAll('.image')
+    const imageIndexes = document.querySelectorAll('.image-index')
+    let activeIndex = 0
+
+    const prev = () => {
+        images[activeIndex].classList.remove('active')
+        imageIndexes[activeIndex].classList.remove('active-image')
+        activeIndex = activeIndex === 0 ? activeIndex = 2 : activeIndex - 1
+        images[activeIndex].classList.add('active')
+        imageIndexes[activeIndex].classList.add('active-image')
+    }
+
+    const next = () => {
+        images[activeIndex].classList.remove('active')
+        imageIndexes[activeIndex].classList.remove('active-image')
+        activeIndex = activeIndex === 2 ? activeIndex = 0 : activeIndex + 1
+        images[activeIndex].classList.add('active')
+        imageIndexes[activeIndex].classList.add('active-image')
+    }
+
+    leftButton.addEventListener('click', prev)
+    rightButton.addEventListener('click', next)
+
 }
-
-/* Slider */ 
-
-const leftButton = document.querySelector('#left-button')
-const rightButton = document.querySelector('#right-button')
-const images = document.querySelectorAll('.image')
-const imageIndexes = document.querySelectorAll('.image-index')
-let activeIndex = 0
-
-const prev = () => {
-    images[activeIndex].classList.remove('active')
-    imageIndexes[activeIndex].classList.remove('active-image')
-    activeIndex = activeIndex === 0 ? activeIndex = 2 : activeIndex - 1
-    images[activeIndex].classList.add('active')
-    imageIndexes[activeIndex].classList.add('active-image')
-}
-
-const next = () => {
-    images[activeIndex].classList.remove('active')
-    imageIndexes[activeIndex].classList.remove('active-image')
-    activeIndex = activeIndex === 2 ? activeIndex = 0 : activeIndex + 1
-    images[activeIndex].classList.add('active')
-    imageIndexes[activeIndex].classList.add('active-image')
-}
-
-leftButton.addEventListener('click', prev)
-rightButton.addEventListener('click', next)
-
